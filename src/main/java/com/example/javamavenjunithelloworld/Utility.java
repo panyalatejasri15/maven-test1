06693bebd0240c2ab4a7fab060b921543b66d137
panyalatejasri15 <panyalatejasrireddy@gmail.com>
2024-09-22 18:16:04 +0530

    TEJASRIP0001-Wrote the code for adding 2 numbers

---
+++ src/main/java/com/example/javamavenjunithelloworld/Utility.java
@@ -0,0 +1,8 @@
+public class Utility {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 20;
+        int c = a + b;
+        System.out.println("Addition of 2 numbers = " + c);
+    }
+}
